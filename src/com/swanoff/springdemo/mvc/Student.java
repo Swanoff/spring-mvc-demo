c886efe50de953281246b2a731505f0425a90e2a
Surya <suryathegreat.07@gmail.com>
2020-02-29 23:58:54 +0530

    Dropdown v2
    Read list of countries from java class

--- src/com/swanoff/springdemo/mvc/Student.java
+++ src/com/swanoff/springdemo/mvc/Student.java
@@ -1,13 +1,25 @@
 package com.swanoff.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
 	private String country;
 	
+	private LinkedHashMap<String, String> countryOptions;
+	
 	public Student() {
 		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("GE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States");
 	}
 
 	public String getFirstName() {
@@ -32,6 +44,10 @@ public class Student {
 
 	public void setCountry(String country) {
 		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 	
 	
